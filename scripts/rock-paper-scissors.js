284d28083c66e1508844091b0bc88d1d9293bc09
eunjistewart <eunjistewart@gmail.com>
2023-08-02 09:53:48 -0400

    update js file

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -37,6 +37,28 @@ function autoPlay() {
   }
 }
 
+document.querySelector(".js-rock-button").addEventListener("click", () => {
+  playGame("rock");
+});
+
+document.querySelector(".js-paper-button").addEventListener("click", () => {
+  playGame("paper");
+});
+
+document.querySelector(".js-scissors-button").addEventListener("click", () => {
+  playGame("scissors");
+});
+
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    playGame("rock");
+  } else if (event.key === "p") {
+    playGame("paper");
+  } else if (event.key === "s") {
+    playGame("scissors");
+  }
+});
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
   /* compare the moves to get the result */
